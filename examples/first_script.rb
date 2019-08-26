9c7ce14706c591c1ed94a3c682cb2c419ce2d2a4
Golias <joao.victor@mobile2you.com.br>
2019-08-26 09:45:13 -0300

    added first example and first exercise

---
+++ examples/first_script.rb
@@ -0,0 +1,17 @@
+# Pega o primeiro argumento passado ao script
+name = ARGV[0]
+
+puts "Hello World, #{name}"
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercises/ex_1_introduction.rb
@@ -0,0 +1,9 @@
+=begin  
+    ENUNCIADO:
+      Faça um programa que recebe dois parâmentros: NOME e IDADE
+      e imprima na tela: "Olá, meu nome é NOME e tenho IDADE anos"
+
+      Ex.:
+        - Input: ruby <path>.rb Golias 23
+        - Output: "Olá, meu nome é Golias e tenho 23 anos"
+=end  
